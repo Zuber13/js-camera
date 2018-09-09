01d667d28d580d24c2a3e91bd0423e82387b0824
unknown <aashusmaileforever@gmail.com>
2018-09-09 12:10:07 +0530

    camra

---
+++ main.js
@@ -0,0 +1,27 @@
+var video = document.querySelector('#video');
+var canvas = document.querySelector('#canvas')
+var context = canvas.getContext('2d');
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.oGetUserMedia || navigator.msGetUserMedia;
+
+if (navigator.getUserMedia) {
+    navigator.getUserMedia({
+        video: true
+    }, streamWebCam, throwError);
+}
+
+function streamWebCam(stream) {
+    video.src = window.URL.createObjectURL(stream);
+    video.play();
+}
+
+function throwError(e) {
+    alert(e.name);
+}
+
+function snap() {
+    canvas.width = video.clientWidth;
+    canvas.height = video.clientHeight;
+    context.drawImage(video, 0, 0);
+}
